1399850f6dc68b077a3eb854d402c3a51b188bf5
rasathelamedude <rasyar.safen19@gmail.com>
2026-02-08 21:10:05 +0300

    Implement insertion operations on circular linked list

--- linked-lists/circular-linked-list.cpp
+++ linked-lists/circular-linked-list.cpp
@@ -9,9 +9,9 @@ using namespace std;
  * EXAMPLE:
  * head -> 0x100: <10, 0x200> -> 0x200: <20, 0x300> -> 0x300: <30, 0x100>
  *
- * It is ideal for tasks like scheduling like round-robin scheduling in operating systems, where you want to store a list of tasks that need to be executed in a specific order. 
+ * It is ideal for tasks like scheduling like round-robin scheduling in operating systems, where you want to store a list of tasks that need to be executed in a specific order.
  * As well as in playlists, where you want to store a list of songs in a specific order.
- * And repeating sequences like Traffic lights (red -> yellow -> green). 
+ * And repeating sequences like Traffic lights (red -> yellow -> green).
  *
  * Circular linked lists can be of both types (singly and doubly)
  * We'll only implement circular singly linked lists since doubly linked lists have already been implemented and the concepts are the same
@@ -38,6 +38,22 @@ private:
     return head == nullptr;
   }
 
+  int getSize() const
+  {
+    int size = 0;
+
+    Node *current = head;
+
+    while (current->next != head)
+    {
+      size++;
+      current = current->next;
+    }
+
+    // Account for the last node
+    return ++size;
+  }
+
 public:
   Node *head;
   Node *tail;
@@ -48,11 +64,70 @@ public:
     tail = nullptr;
   }
 
-  void insertAtBeginning(int value) {}
+  void insertAtBeginning(int value)
+  {
+    Node *newNode = new Node(value);
 
-  void insertAtEnd(int value) {}
+    if (isEmpty())
+    {
+      head = newNode;
+      tail = newNode;
+      tail->next = head;
+    }
+    else
+    {
+      newNode->next = head;
+      tail->next = newNode;
+      head = newNode;
+    }
+  }
 
-  void insertAtPosition(int position, int value) {}
+  void insertAtEnd(int value)
+  {
+    Node *newNode = new Node(value);
+
+    if (isEmpty())
+    {
+      head = newNode;
+      tail = newNode;
+      tail->next = head;
+    }
+    else
+    {
+      tail->next = newNode;
+      tail = newNode;
+      tail->next = head;
+    }
+  }
+
+  void insertAtPosition(int position, int value)
+  {
+    if (position == 0)
+    {
+      insertAtBeginning(value);
+    }
+
+    if (position == getSize())
+    {
+      insertAtEnd(value);
+    }
+
+    if (position > getSize())
+    {
+      cout << "Position exceeds list size" << endl;
+    }
+
+    Node *newNode = new Node(value);
+    Node *current = head;
+
+    for (int i = 0; i < position - 1; i++)
+    {
+      current = current->next;
+    }
+
+    newNode->next = current->next;
+    current->next = newNode;
+  }
 
   void deleteAtBeginning() {}
 
@@ -67,6 +142,13 @@ public:
 
 int main()
 {
-  cout << "Circular linked list" << endl;
+  CircularLinkedList list;
+
+  list.insertAtBeginning(10);
+  list.insertAtBeginning(20);
+  list.insertAtBeginning(30);
+
+  list.insertAtPosition(1, 40);
+
   return 0;
 }
