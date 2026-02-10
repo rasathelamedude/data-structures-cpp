c155fc88aa08ee1c4963dee970f7241476a7aefc
rasathelamedude <rasyar.safen19@gmail.com>
2026-02-10 18:48:42 +0300

    Implement an array-based stack

--- stack/array-stack.cpp
+++ stack/array-stack.cpp
@@ -2,36 +2,79 @@
 using namespace std;
 
 /**
- * 
+ *
  * A stack is a linear data structure that stores items in a Last-In-First-Out (LIFO) manner.
  * This means that the last item added to the stack is the first one to be removed.
- * 
+ *
  * It is used in stynatx parsing and expression evaluation.
  * It is used to store function calls in a function call stack.
  * It's also thanks to stacks that we can implement recursive functions.
- * 
+ *
  */
 
-class Stack {
+class Stack
+{
   // The stack property here lives on the heap
+private:
+  int top;
+  int *stack;
+  int size;
 
-  private:
-    int top;
-    int *stack;
+public:
+  Stack(int size)
+  {
+    this->size = size;
+    this->top = -1;
+    this->stack = new int[size];
+  }
 
-    bool isEmpty() {
-      return top == -1;
+  ~Stack()
+  {
+    delete[] this->stack;
+  }
+
+  bool isEmpty()
+  {
+    return this->top == -1;
+  }
+
+  bool isFull()
+  {
+    return this->top == this->size - 1;
+  }
+
+  void push(int value)
+  {
+    if (this->isFull())
+    {
+      cout << "Stack is full" << endl;
+      return;
     }
 
-  public:
-    Stack(int size) {
-      this->top = -1;
-      this->stack = new int[size];
+    this->top++;
+    this->stack[this->top] = value;
+  }
+
+  int pop()
+  {
+    if (this->isEmpty())
+    {
+      cout << "Stack is empty" << endl;
+      return -1;
     }
 
-    ~Stack() {
-      delete[] this->stack;
+    return this->stack[this->top--];
+  }
+
+  int peek()
+  {
+    if (this->isEmpty())
+    {
+      return -1;
     }
+
+    return this->stack[this->top];
+  }
 };
 
 int main()
